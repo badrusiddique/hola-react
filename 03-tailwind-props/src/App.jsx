6f06bc82b1df400eb90062a75019aa5f3e52541c
Badru <badru@buckytechnologies.com>
2025-12-30 13:01:58 +0530

    tailwind props

---
+++ 03-tailwind-props/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import Card from './components/Card.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl rounded-xl bg-green-500 mb-4">03 Tailwind Props</h1>
+      {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="vite.svg" alt="" width="384" height="512" />
+        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p class="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption class="font-medium">
+            <div class="text-sky-500 dark:text-sky-400">
+              Sarah Dayan
+            </div>
+            <div class="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure> */}
+
+
+      <Card title="Lorem" />
+      <Card title="Ipsum" />
+    </>
+  )
+}
+
+export default App
